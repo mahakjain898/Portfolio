cf9b2455f76010cd0c0ff0884cd82ceb30445830
mahakjain898 <44893117+mahakjain898@users.noreply.github.com>
2024-10-09 14:30:35 -0400

    spacing

--- src/components/Container.js
+++ src/components/Container.js
@@ -3,7 +3,7 @@ import React from "react";
 
 const Container = ({ children }) => {
   return (
-    <div className="flex m-2 py-2 font-light border-2 rounded-[24px] border-black text-xl justify-between w-[1084px] h-[68px] text-black">
+    <div className=" flex m-2 py-2 font-light bg-gray-300 bg-opacity-20 rounded-[24px] text-xl justify-between w-[1084px] h-[68px] text-black">
       {" "}
       {/* Added text-black here */}
       {children} {/* This will render the content passed to the Container */}
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,30 +3,32 @@ import Container from "./Container";
 
 const Header = () => {
   return (
-    <Container>
-      <div className="flex justify-between items-center w-full h-full text-black sticky ">
-        <div className="flex items-center p-2">
-          <div className="m-2">Mahak Jain</div>
-        </div>
+    <div className="pt-8">
+      <Container>
+        <div className="flex justify-between items-center w-full h-full sticky ">
+          <div className="flex items-center p-2">
+            <div className="m-2">Mahak Jain</div>
+          </div>
 
-        <div className="flex items-center">
-          <ul className="flex">
-            <li className="px-6 py-4 transition-transform duration-200 ease-in-out transform hover:scale-110 hover:text-orange-300 hover:cursor-pointer">
-              Home
-            </li>
-            <li className="px-6 py-4 transition-transform duration-200 ease-in-out transform hover:scale-110 hover:text-orange-300 hover:cursor-pointer">
-              About
-            </li>
-            <li className="px-6 py-4 transition-transform duration-200 ease-in-out transform hover:scale-110 hover:text-orange-300 hover:cursor-pointer">
-              Project
-            </li>
-            <li className="px-6 py-4 transition-transform duration-200 ease-in-out transform hover:scale-110 hover:text-orange-300 hover:cursor-pointer">
-              Resume
-            </li>
-          </ul>
+          <div className="flex items-center">
+            <ul className="flex">
+              <li className="px-6 py-4 transition-transform duration-200 ease-in-out transform hover:scale-110 hover:text-orange-300 hover:cursor-pointer">
+                Home
+              </li>
+              <li className="px-6 py-4 transition-transform duration-200 ease-in-out transform hover:scale-110 hover:text-orange-300 hover:cursor-pointer">
+                About
+              </li>
+              <li className="px-6 py-4 transition-transform duration-200 ease-in-out transform hover:scale-110 hover:text-orange-300 hover:cursor-pointer">
+                Project
+              </li>
+              <li className="px-6 py-4 transition-transform duration-200 ease-in-out transform hover:scale-110 hover:text-orange-300 hover:cursor-pointer">
+                Resume
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </Container>
+      </Container>
+    </div>
   );
 };
 
