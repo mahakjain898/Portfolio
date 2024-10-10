ce5cf819071cd92c84f669b80f006441f2f1fbaa
mahakjain898 <44893117+mahakjain898@users.noreply.github.com>
2024-10-09 23:20:07 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
 import Body from "./components/Body";
 import Header from "./components/Header";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./components/Home";
 function App() {
   return (
     <div className="flex justify-center items-start min-h-screen bg-gradient-to-t from-white to-[#FFFDF6] ">
       <div className="w-[1084px]">
         <Header />
+        {/* <Home /> */}
         <Body />
       </div>
     </div>
